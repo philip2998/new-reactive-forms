0d15fbb3296abefffc5608493b60240271033ee0
Philip Gevorgyan <filipgevorgyan@gmail.com>
2023-12-05 14:38:54 +0400

    Added initial files of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { HeaderComponent } from './header/header.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    HeaderComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
